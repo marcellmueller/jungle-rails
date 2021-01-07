aed00b0597de78c745a816d4b4fdc5f315c0f82a
marcellmueller <mlmueller@protonmail.com>
2021-01-06 18:05:41 -0800

    New sale page is completed!

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -4,4 +4,27 @@ class Admin::SalesController < ApplicationController
     @sales = Sale.all
   end
 
+  def new
+    @admin_sale = Sale.new
+  end
+
+  def create
+    @admin_sale = Sale.new(sale_params)
+  
+    if @admin_sale.save
+      redirect_to [:admin, :sales], notice: 'Sale Created!'
+    else
+      render :new
+    end
+  end
+
+  def sale_params
+    params.require(:sale).permit(
+      :name,
+      :percent_off,
+      :starts_on,
+      :ends_on
+    )
+  end
 end
+
