09a948e4f3869259a9a347eada446083475a94f1
marcellmueller <mlmueller@protonmail.com>
2021-01-11 10:20:18 -0800

    Finished setting up product tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+require 'pp'
+RSpec.describe Product, type: :model do
+
+  describe 'Validations' do
+
+    describe "name" do
+    
+      it "Should create new product" do
+        @category = Category.create(name: 'foo')
+        @product = Product.new(name: 'bar', price_cents: 10, quantity: 10, category_id: @category.id)
+        expect(@product.valid?).to equal(true)
+        @product.valid?
+        expect(@product.errors.full_messages).to include()
+        
+      end
+
+      it "Should respond with error if name is null" do
+        @category = Category.create(name: 'foo')
+        @product = Product.new(name: nil, price_cents: 10, quantity: 10, category_id: @category.id)  
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Name can't be blank"
+        )
+      end
+
+      it "Should respond with error if price is null" do
+        @category = Category.create(name: 'foo')
+        @product = Product.new(name: 'bar', price_cents: nil, quantity: 10, category_id: @category.id)
+        @product.price_cents = nil
+         @product.valid?
+         expect(@product.errors.full_messages).to include("Price cents is not a number"
+         )
+       end
+
+       it "Should respond with error if quantity is null" do
+        @category = Category.create(name: 'foo')
+        @product = Product.new(name: 'bar', price_cents: 10, quantity: nil, category_id: @category.id)
+        @product.quantity = nil
+        @product.valid?
+         expect(@product.errors.full_messages).to include("Quantity can't be blank")
+       end
+
+       it "Should respond with error if name is null" do
+        @category = Category.create(name: 'foo')
+        @product = Product.new(name: 'bar', price_cents: 10, quantity: 10, category_id: nil)
+        @product.category_id = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Category can't be blank")
+      end
+
+
+    end
+  end
+end
